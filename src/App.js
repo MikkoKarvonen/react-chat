923243be17710f4261fbf89090d3e6a6c9627b69
MikkoKarvonen <mkarvonen34@gmail.com>
2019-02-21 17:00:49 +0200

    Add charecter deletion

--- src/App.js
+++ src/App.js
@@ -5,19 +5,25 @@ class App extends Component {
   constructor(props) {
     super(props);
     this.state = {
-      value: ''
+      value: '',
+      chatText: [...Array(200)].map(i=>(~~(Math.random()*36)).toString(36)).join('')
     };
     this.handleChange = this.handleChange.bind(this);
   }
 
   handleChange(event) {
-    this.setState({value: event.target.value});
+    let temp = this.state.chatText + event.target.value
+    if (temp.length >= 200){
+      temp = temp.substr(1);
+      this.setState({chatText: temp});
+    }
+    this.setState({value: ''});
   }
 
   render() {
     return (
       <div>
-        <div className="chatBox">{this.state.value}</div>
+        <div className="chatBox">{this.state.chatText}</div>
         <input type="text" value={this.state.value} onChange={this.handleChange} />
       </div>
     );
